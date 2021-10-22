d71a29b25050c2fe5313f0b36b62db9f91337951
Alyssa DeMay <camocat11@gmail.com>
2021-10-21 21:38:56 -0500

    Instagram clone set up

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.sass"
+import Navigation from "./Navigation"
+import Menu from './Menu'
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation />
+    </div>
+  );
+}
+
+export default App;
