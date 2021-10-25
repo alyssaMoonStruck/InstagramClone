ac3ccb2d272fff2cf95134c6c017ea0b83cc5059
Alyssa DeMay <camocat11@gmail.com>
2021-10-25 14:58:48 -0500

    Adding SCSS to Cards

---
+++ src/components/CardMenu.js
@@ -0,0 +1,14 @@
+import "../styles/cardMenu.scss";
+import { ReactComponent as Inbox } from "../images/inbox.svg";
+import { ReactComponent as Comments } from "../images/comments.svg";
+import { ReactComponent as Notifications } from "../images/notifications.svg";
+import { ReactComponent as Bookmark } from "../images/bookmark.svg";
+
+function CardMenu() {
+    return (
+    <div className="cardMenu">
+
+    </div>)
+}
+
+export default CardMenu;
